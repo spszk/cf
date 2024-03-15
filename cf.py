def20e5be314c30c7205100bbbd431e0750ebb32
Cody <bad14@outlook.it>
2024-03-15 16:51:13 +0100

    update

--- cf.py
+++ cf.py
@@ -1,4 +1,5 @@
 import pandas
+from datetime import datetime
 
 vowels = "aeiou"
 nums = "0123456789"
@@ -6,68 +7,47 @@ temp_list = []
 
 # last name
 surname = input("What is your last name? ")
-cf_surname = ""
-for i in range(len(surname)):
-    if surname[i].lower() in vowels:
-        temp_list.append(i)
-    elif surname[i] == " ":
-        pass
-    else:
-        cf_surname += surname[i].upper()
-if len(cf_surname) > 3:
-    cf_surname = cf_surname[0:3]
-elif len(cf_surname) < 3:
-    i = 0
-    while len(cf_surname) < 3 and i < len(temp_list):
-        cf_surname += surname[temp_list[i]].upper()
-        i += 1
-    while len(cf_surname) < 3:
-        cf_surname += "X"
+
+temp_list = [letter.upper() for letter in surname if letter.lower() not in vowels]
+temp_list.extend([letter.upper() for letter in surname if letter.lower() in vowels])
+while len(temp_list) < 3:
+    if [] in temp_list:
+        temp_list.pop(temp_list.index([]))
+    temp_list.append('X')
+
+cf_surname = "".join(temp_list[:3])
 temp_list.clear()
+
 
 # name
 name = input("What is your name? ")
-cf_name = ""
-for i in range(len(name)):
-    if name[i].lower() in vowels:
-        temp_list.append(i)
-    elif name[i] == " ":
-        pass
-    else:
-        cf_name += name[i].upper()
-if len(cf_name) > 3:
-    cf_name = cf_name[0] + cf_name[2:4]
-elif len(cf_name) < 3:
-    i = 0
-    while len(cf_name) < 3 and i < len(temp_list):
-        cf_name += name[temp_list[i]].upper()
-        i += 1
-    while len(cf_name) < 3:
-        cf_name += "X"
+temp_list = [letter.upper() for letter in name if letter.lower() not in vowels]
+temp_list.extend([letter.upper() for letter in name if letter.lower() in vowels])
+temp_list = list(filter(lambda l: l.isalnum(), temp_list))
+
+while len(temp_list) < 3:
+    temp_list.append('X')
+
+cf_name = "".join([temp_list[0], temp_list[2], temp_list[3]])
 temp_list.clear()
 
+
 # date and sex
-sex = input("What's your sex? F or M?")
-birth = input("When were you born? DDMMYYYY")
-months = "ABCDEHLMPRST"
-cf_day = 0
-cf_month = ""
-cf_year = 0
-
-if sex != "F" and sex != "f" and sex != "M" and sex != "m":
+sex = input("What's your sex? F or M? ")
+while sex != "F" and sex != "f" and sex != "M" and sex != "m":
     sex = input("Sex not valid. What's your sex? ")
-elif sex == "F" or sex == "f":
-    cf_day += 40
 
-while len(birth) > 8 or len(birth) < 8:
-    birth = input("Date format not valid. When were you born? DDMMYYYY ")
-while int(birth[2:4]) > 12:
-    birth = input("Impossible month. When were you born? DDMMYYYY ")
-while int(birth[0:2]) > 31 or int(birth[0:2]) < 1:
-    birth = input("Impossible day. When were you born? DDMMYYYY ")
-cf_day += int(birth[0:2])
-cf_month = months[int(birth[2:4]) - 1]
-cf_year = birth[6:8]
+birth = datetime.strptime(input("When were you born? DD-MM-YYYY "), '%d-%m-%Y')
+while birth == ValueError:
+    birth = datetime.strptime(input("Invalid date. When were you born? DD-MM-YYYY "), '%d-%m-%Y')
+
+months = "ABCDEHLMPRST"
+cf_day = birth.day
+cf_month = months[birth.month - 1]
+cf_year = datetime.strftime(birth, '%y')
+
+if sex == "F" or sex == "f":
+    cf_day += 40
 
 # city
 city = input("Where were you born? If you were born outside of Italy, just type the country. ").upper()
@@ -77,7 +57,7 @@ while cf_city.to_string(index=False) == "Series([], )":
     cf_city= input("Invalid birthplace. Where were you born? ")
 
 # control
-cf_data = cf_surname + cf_name + cf_year + cf_month + str(cf_day) + cf_city.to_string(index=False)
+cf_data = cf_surname + cf_name + str(cf_year) + cf_month + str(cf_day) + cf_city.to_string(index=False)
 odds = pandas.read_csv("odds.csv")
 evens = pandas.read_csv("evens.csv")
 codify = pandas.read_csv("codify.csv")
